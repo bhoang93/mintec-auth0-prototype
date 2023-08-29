92762af8e689254017b21411cf74bc63cd3d82ba
Brian Hoang <39828877+bhoang93@users.noreply.github.com>
2023-08-29 11:51:16 +0100

    Update login-button.js

--- src/components/buttons/login-button.js
+++ src/components/buttons/login-button.js
@@ -2,10 +2,10 @@ import { useAuth0 } from "@auth0/auth0-react";
 import React from "react";
 
 export const LoginButton = () => {
-  const { loginWithRedirect } = useAuth0();
+  const { loginWithPopup } = useAuth0();
 
   const handleLogin = async () => {
-    await loginWithRedirect({
+    await loginWithPopup({
       prompt: "login",
       appState: {
         returnTo: "/profile",
