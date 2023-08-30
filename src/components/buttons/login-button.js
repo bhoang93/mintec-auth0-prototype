f174d41d98c8917c89ef310678f5ad28f4205713
Brian Hoang <brian.hd.hoang@gmail.com>
2023-08-30 13:57:55 +0100

    modal

--- src/components/buttons/login-button.js
+++ src/components/buttons/login-button.js
@@ -1,21 +1,36 @@
-import { useAuth0 } from "@auth0/auth0-react";
-import React from "react";
+// import { useAuth0 } from "@auth0/auth0-react";
+import React, {useState} from "react";
 
 export const LoginButton = () => {
-  const { loginWithPopup } = useAuth0();
+  // const { loginWithPopup } = useAuth0();
 
-  const handleLogin = async () => {
-    await loginWithPopup({
-      prompt: "login",
-      appState: {
-        returnTo: "/profile",
-      },
-    });
-  };
+  // const handleLogin = async () => {
+  //   await loginWithPopup({
+  //     prompt: "login",
+  //     appState: {
+  //       returnTo: "/profile",
+  //     },
+  //   });
+  // };
+
+  const LoginModal = () => {
+    return(
+      <div style={{ position: "absolute", top: 250, left: 750, zIndex: 999 }}>
+      <iframe src="https://https://dev-brian-codurance.eu.auth0.com//authorize?response_type=token&client_id=DH8mvZrs37rADw5hH0zUQ9qdbH4ilbHr&redirect_uri=https://mintec-auth0-prototype.vercel.app" title="login" />
+      </div>
+    )
+  }
+
+  const [showLoginModal, toggleLoginModal] = useState(false);
 
   return (
-    <button className="button__login" onClick={handleLogin}>
+    <div>
+    <button className="button__login" onClick={() => toggleLoginModal(true)}>
       Log In
     </button>
+    {
+      showLoginModal && <LoginModal />
+    }
+    </div>
   );
 };
